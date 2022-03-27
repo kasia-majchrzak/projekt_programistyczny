223665ae349eb1ec69ad8886c6ebeda5731f3e16
kasia-majchrzak <katarzyna.majchrzak1@edu.uekat.pl>
2022-03-27 09:07:39 +0200

    Modified text preprocessing

--- app/services/PreprocessingService.py
+++ app/services/PreprocessingService.py
@@ -3,14 +3,14 @@ from os import read
 
 
 class PreprocessingService:
-    COMMON_WORDS_PATH="..\\..\\static\\txt\\commonWords.txt"
+    COMMON_WORDS_PATH = "..\\..\\static\\txt\\commonWords.txt"
 
     def preprocess_text(self, input_text: str) -> []:
         input_text = input_text.lower()
         re.sub('[^A-Za-z0-9]+', '', input_text)
         input_text_tokens = input_text.split(" ")
 
-        common_words_file = open(COMMON_WORDS_PATH).read()
+        common_words_file = open(self.COMMON_WORDS_PATH).read()
         if len(common_words_file) > 0:
             for word in common_words_file.split(","):
                 input_text_tokens = input_text_tokens.remove(word)
